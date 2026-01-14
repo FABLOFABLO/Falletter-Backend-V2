bdf08a1b4cd2e2b27366778d378c64d549afc57e
김지윤 <jjunkim@dsm.hs.kr>
2026-01-15 08:56:49 +0900

    initial commit

---
+++ src/main/java/com/example/falleterbev2/Falletterbev2Application.java
@@ -0,0 +1,13 @@
+package com.example.falleterbev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Falletterbev2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Falletterbev2Application.class, args);
+	}
+
+}
