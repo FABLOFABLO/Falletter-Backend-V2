86d625e319bbce13fe86ea7d27b8c9683b9cf1b1
김지윤 <jjunkim@dsm.hs.kr>
2026-02-02 18:42:05 +0900

    feat: UserFacade 추가

---
+++ src/main/java/com/example/falleterbev2/domain/user/facade/UserFacade.java
@@ -0,0 +1,24 @@
+package com.example.falleterbev2.domain.user.facade;
+
+import com.example.falleterbev2.domain.user.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+@Component
+@RequiredArgsConstructor
+public class UserFacade {
+    private final UserRepository userRepository;
+
+    public Long currentUserId() {
+        String userName = SecurityContextHolder.getContext().getAuthentication(). getName();
+
+        return userRepository.findByName(userName)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "사용자를 찾을 수 없습니다."
+                )).getId();
+    }
+}
