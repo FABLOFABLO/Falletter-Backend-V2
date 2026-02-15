bbbc5383e89dd5c0fec098f0c3db9d99f03f74bd
Kanghee <kh63167574@gmail.com>
2026-02-15 19:49:20 +0900

    refactor : Letter DTO 통합

--- src/main/java/com/example/falleterbev2/domain/letter/presentation/dto/request/LettersRequest.java
+++ src/main/java/com/example/falleterbev2/domain/letter/presentation/dto/request/LettersRequest.java
@@ -1,13 +1,12 @@
 package com.example.falleterbev2.domain.letter.presentation.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class LettersSendRequest {
+public class LettersRequest {
 
     @NotBlank(message = "receiverId는 필수입니다.")
     private Long receiverId;
--- src/main/java/com/example/falleterbev2/domain/letter/service/LetterReadSentService.java
+++ src/main/java/com/example/falleterbev2/domain/letter/service/LetterReadSentService.java
@@ -3,20 +3,18 @@ package com.example.falleterbev2.domain.letter.service;
 import com.example.falleterbev2.domain.letter.domain.Letter;
 import com.example.falleterbev2.domain.letter.domain.repository.LetterRepository;
 import com.example.falleterbev2.domain.letter.exception.NotFoundSentLetterException;
-import com.example.falleterbev2.domain.letter.presentation.dto.response.LetterReadSentResponse;
+import com.example.falleterbev2.domain.letter.presentation.dto.response.LettersResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class LetterReadSentService {
     private final LetterRepository letterRepository;
 
-    @Transactional
-    public LetterReadSentResponse execute(Long id) {
+    public LettersResponse execute(Long id) {
         Letter letter = letterRepository.findById(id)
                 .orElseThrow(() -> NotFoundSentLetterException.EXCEPTION);
-        return new LetterReadSentResponse(letter);
+        return new LettersResponse(letter);
     }
 }
