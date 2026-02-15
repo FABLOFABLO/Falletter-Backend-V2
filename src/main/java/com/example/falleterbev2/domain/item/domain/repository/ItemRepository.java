26d18671f92da32827d8851112696ec2b2f46d4d
김지윤 <jjunkim@dsm.hs.kr>
2026-02-15 17:04:21 +0900

    refactor: repository 어노테이션 삭제

--- src/main/java/com/example/falleterbev2/domain/item/domain/repository/ItemRepository.java
+++ src/main/java/com/example/falleterbev2/domain/item/domain/repository/ItemRepository.java
@@ -2,10 +2,8 @@ package com.example.falleterbev2.domain.item.domain.repository;
 
 import com.example.falleterbev2.domain.item.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
-@Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
     Optional<Item> findByUserId(Long userId);
 }
