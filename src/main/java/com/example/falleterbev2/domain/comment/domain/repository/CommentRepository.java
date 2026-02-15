6ddb7fafc28b8bcda9a0e850a4ceca48d6d85759
Kanghee <kh63167574@gmail.com>
2026-02-15 19:27:03 +0900

    refactor : 레포지토리 어노테이션 제거

--- src/main/java/com/example/falleterbev2/domain/comment/domain/repository/CommentRepository.java
+++ src/main/java/com/example/falleterbev2/domain/comment/domain/repository/CommentRepository.java
@@ -2,9 +2,7 @@ package com.example.falleterbev2.domain.comment.domain.repository;
 
 import com.example.falleterbev2.domain.comment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
 }
