8c544dce613589735415e7aa470568c5ab193855
Kanghee <kh63167574@gmail.com>
2026-02-16 12:33:14 +0900

    refactor: repository import 삭제

--- src/main/java/com/example/falleterbev2/domain/letter/domain/repository/LetterRepository.java
+++ src/main/java/com/example/falleterbev2/domain/letter/domain/repository/LetterRepository.java
@@ -3,7 +3,6 @@ package com.example.falleterbev2.domain.letter.domain.repository;
 import com.example.falleterbev2.domain.letter.domain.Letter;
 import com.example.falleterbev2.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import java.util.List;
 
 public interface LetterRepository extends JpaRepository<Letter, Long> {
